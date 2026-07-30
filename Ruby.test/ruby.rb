20f204f02d92a6ff687eece17e3b70fa077d311a
yuki25642349-arch <yuki25642349@gmail.com>
2026-07-30 17:19:39 +0900

    処理調整

--- Ruby.test/ruby.rb
+++ Ruby.test/ruby.rb
@@ -3,17 +3,23 @@ p "1" + "1"
 p 1 + 1
 p 4 + 2
 p 4 * 2
+
 tmp = "1" #変数tmpに"１"を代入
+
 p tmp #"1"
+
 a = "ruby"
 b = "入門"
+
 p a + b
+
 program = "ruby"
+
 p program + "入門"
 p program + "講座"
 p "第#{tmp}回#{program}講座"
 p 1 == 1
 p 1 == 2
+
 arr = ["ruby","php","Python"]
 if score = 80
-    
