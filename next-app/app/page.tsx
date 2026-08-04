8616fae1b64cd12ed208f2856642946ec2f159c1
神崎絢人 <yuki25642349@gmail.com>
2026-08-05 03:11:59 +0900

    2026/08/05 Next.js記録

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
-  return (
+type QiitaResponse = {
+  id: string;
+  title: string;
+  url: string;
+  image: string;
+};
+
+export default async function Home() {
+  const getQiitaItems = async () => {
+    const responce = await axios.get<QiitaResponse[]>("https://qiita.com/api/v2/authenticated_user/items", 
+      {
+      headers: {
+        Authorization: `Bearer ${process.env.QIITA_API_KEY}`
+      },
+    }
+  );
+  return responce.data;
+};
+const qiitaItems = await getQiitaItems();
+  return(
     <div>
       <h1>Welcome to Next.js!</h1>
+      <ul>
+        {qiitaItems.map((item) => (
+          <li key={item.id}>
+            <a href={item.url} target="_blank" rel="noopener noreferrer">
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
