d436a04455240fb8a31a486ce196083f4b45219e
神崎絢人 <yuki25642349@gmail.com>
2026-09-08 04:22:27 +0900

    2026/09/08/フォントファイルをtsxで読み込む

---
+++ portfolio/app/fonts.ts
@@ -0,0 +1,6 @@
+import { Yuji_Boku } from "next/font/google";
+
+export const yujiBoku = Yuji_Boku({
+  weight: "400",
+  subsets: ["latin"],
+});
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { yujiBoku } from "./fonts";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -62,7 +62,7 @@ export default function Home() {
         <div>
           <p>WEB APPLICATION DEVELOPER</p>
 
-          <h1 className="reveal">
+          <h1 className={`reveal ${yujiBoku.className}`}>
             一つ一つの
             <br />
             <span>学びを</span>
